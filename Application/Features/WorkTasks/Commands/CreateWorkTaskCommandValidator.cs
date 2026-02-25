b4e363025b486778fd68f95de7882a22a124de90
Darkness_696 <darkness_696@mail.ru>
2026-02-25 12:42:21 +0300

    added CreateWorkTaskCommandValidator

---
+++ Application/Features/WorkTasks/Commands/CreateWorkTaskCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.WorkTasks.Commands
+{
+    public class CreateWorkTaskCommandValidator : AbstractValidator<CreateWorkTaskCommand>
+    {
+        public CreateWorkTaskCommandValidator()
+        {
+            RuleFor(x => x.TaskDto.Title).NotEmpty();
+            RuleFor(x => x.TaskDto.DueDate).GreaterThan(DateTime.Now);
+        }
+    }
+}
